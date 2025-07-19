e23747dbbd2f9c73f34533c56d6a7857d6b28d56
Christof Fetzer <fetzer@scontain.com>
2025-07-19 04:13:14 +0000

    fix: making script idempotent and fixing port-forward

--- scripts/install_prometheus_grafana.sh
+++ scripts/install_prometheus_grafana.sh
@@ -122,34 +122,50 @@ kubectl apply --server-side -n monitoring -f prometheus-grafana-manifests/config
 
 current_volumes=$(kubectl get deployment -n monitoring grafana -o json | jq -e -r '.spec.template.spec.volumes')
 
-updates_volumes=$(echo $current_volumes | jq '. += [
-  {
-    "configMap": {
-      "defaultMode": 420,
+# Check if the volume with name "grafana-scone-dashboard-operator" already exists
+if ! echo "$current_volumes" | jq -e '.[] | select(.name == "grafana-scone-dashboard-operator")' > /dev/null; then
+  # Add the new volumes if not already present
+  updates_volumes=$(echo "$current_volumes" | jq '. += [
+    {
+      "configMap": {
+        "defaultMode": 420,
+        "name": "grafana-scone-dashboard-operator"
+      },
       "name": "grafana-scone-dashboard-operator"
     },
-    "name": "grafana-scone-dashboard-operator"
-  },
-  {
-    "configMap": {
-      "defaultMode": 420,
+    {
+      "configMap": {
+        "defaultMode": 420,
+        "name": "grafana-scone-dashboard-runtime"
+      },
       "name": "grafana-scone-dashboard-runtime"
-    },
-    "name": "grafana-scone-dashboard-runtime"
-  }
-]')
+    }
+  ]')
+else
+  # Leave it unchanged
+  updates_volumes="$current_volumes"
+fi
+
 
 current_volume_mounts=$(kubectl get deployment -n monitoring grafana -o json | jq -e -r '.spec.template.spec.containers[0].volumeMounts')
-updates_volume_mounts=$(echo $current_volume_mounts | jq '. += [
-  {
-    "mountPath": "/grafana-dashboard-definitions/0/grafana-scone-dashboard-operator",
-    "name": "grafana-scone-dashboard-operator"
-  },
-  {
-    "mountPath": "/grafana-dashboard-definitions/0/grafana-scone-dashboard-runtime",
-    "name": "grafana-scone-dashboard-runtime"
-  }
-]')
+
+# Check if the mountPath exists in current_volume_mounts
+if ! echo "$current_volume_mounts" | jq -e '.[] | select(.mountPath == "/grafana-dashboard-definitions/0/grafana-scone-dashboard-operator")' > /dev/null; then
+  # Add the new volume mounts if not already present
+  updates_volume_mounts=$(echo "$current_volume_mounts" | jq '. += [
+    {
+      "mountPath": "/grafana-dashboard-definitions/0/grafana-scone-dashboard-operator",
+      "name": "grafana-scone-dashboard-operator"
+    },
+    {
+      "mountPath": "/grafana-dashboard-definitions/0/grafana-scone-dashboard-runtime",
+      "name": "grafana-scone-dashboard-runtime"
+    }
+  ]')
+else
+  # Leave it unchanged
+  updates_volume_mounts="$current_volume_mounts"
+fi
 
 kubectl patch deployment -n monitoring grafana --type=json --patch """
   [
