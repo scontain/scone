0d5b6372925a2e9b74f2ee6c28fe1dd43ab5c7ba
Christof Fetzer <fetzer@scontain.com>
2025-08-06 11:32:39 +0000

    feat: add golang support and example

--- scripts/extract-all-scripts.sh
+++ scripts/extract-all-scripts.sh
@@ -9,6 +9,7 @@ files=(
   "CAS.md scripts/install_cas.sh"
   "prerequisite_check.md scripts/prerequisite_check.sh"
   "scone_monitoring.md scripts/install_prometheus_grafana.sh"
+  "golang.md scripts/run_golang.sh"
   # TODO: uncomment it: "deploying-osv-scanner.md scripts/install_osv_scanner.sh"
 )
 
